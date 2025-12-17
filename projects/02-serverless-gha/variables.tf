90ed97b6787418cba075b1c61f41f272afb67fb5
necatihan <necatihanresay@gmail.com>
2025-12-17 10:31:38 +0300

    feat(serverless): add GitHub OIDC role and terraform workflow

---
+++ projects/02-serverless-gha/variables.tf
@@ -0,0 +1,25 @@
+variable "aws_region" {
+  type    = string
+  default = "eu-north-1"
+}
+
+variable "aws_profile" {
+  type    = string
+  default = "portfolio"
+}
+
+variable "project" {
+  type    = string
+  default = "aws-devops-terraform-portfolio"
+}
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
+variable "repo" {
+  type        = string
+  description = "GitHub repo in the form owner/repo"
+  default     = "necatihan/aws-devops-terraform-portfolio"
+}
