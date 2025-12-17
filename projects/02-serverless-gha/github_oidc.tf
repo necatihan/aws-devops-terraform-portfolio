47022d6eac065a0b96aef3663e23d790866fe55e
necatihan <necatihanresay@gmail.com>
2025-12-17 10:42:10 +0300

    fix(serverless): allow GitHub OIDC from pull_request and main

--- projects/02-serverless-gha/github_oidc.tf
+++ projects/02-serverless-gha/github_oidc.tf
@@ -33,7 +33,10 @@ data "aws_iam_policy_document" "gha_assume_role" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.repo}:ref:refs/heads/main"]
+      values   = [
+        "repo:${var.repo}:ref:refs/heads/main",
+        "repo:${var.repo}:pull_request"
+      ]
     }
   }
 }
