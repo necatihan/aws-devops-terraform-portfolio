db8f6215624b0312ead980609786c16ea74e052d
necatihan <necatihanresay@gmail.com>
2025-12-19 12:17:55 +0300

    feat(ecs): deploy fargate service behind ALB

---
+++ projects/03-ecs-codepipeline/alb.tf
@@ -0,0 +1,38 @@
+resource "aws_lb" "app" {
+  name               = "${local.short}-alb"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+}
+
+resource "aws_lb_target_group" "app" {
+  name        = "${local.short}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    path                = "/"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    interval            = 15
+    timeout             = 5
+    matcher             = "200-399"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
+
+output "alb_url" {
+  value = "http://${aws_lb.app.dns_name}"
+}
